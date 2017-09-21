a9da80bbca501dc3ceca8aa2d1581a4cd5415fe8
andrerobbe <mauritsrobbe@hotmail.com>
2017-09-21 12:52:43 +0200

    git init

---
+++ opdracht-comments/index.php
@@ -0,0 +1,21 @@
+<?php
+	$first_name = 'Maurits';
+	$name = 'Robbe';
+
+	/*
+	Naam = André Maurits Robbe
+	email = andrerobbe@student.kdg.be
+	*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>opdracht-comments</title>
+</head>
+<body>
+	<?php echo $first_name?>
+	<?php echo $name ?>
+</body>
+</html>
