fc158f2f7624a69f273e30dd79f4b7f0733323ab
andrerobbe <mauritsrobbe@hotmail.com>
2017-10-05 12:48:54 +0200

    statements-switch

---
+++ conditional-statement-switch/switch.php
@@ -0,0 +1,41 @@
+<?php
+$getal = 3;
+$string = '';
+
+switch ($getal) { 
+	case 1:
+		$string = 'maandag';
+		break;
+	case 2:
+		$string = 'dinsdag';
+		break;
+	case 3:
+		$string = 'woensdag';
+		break;
+	case 4:
+		$string = 'donderdag';
+		break;
+	case 5:
+		$string = 'vrijdag';
+		break;
+	case 6:
+		$string = 'zaterdag';
+		break;
+	case 7:
+		$string = 'zondag';
+		break;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>if-else</title>
+</head>
+<body>
+	<p><?php echo $getal ?> is <?php echo $string ?></p>
+	
+</body>
+</html>
