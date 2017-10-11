2803650f4cf31fa612658171e0f8894c4b6f8aa2
andrerobbe <mauritsrobbe@hotmail.com>
2017-10-11 19:39:19 +0200

    functions-recursive

---
+++ functions-recursive/recursive-1.php
@@ -0,0 +1,43 @@
+<?php
+	$money = 100000;
+	$interest = 1.08;
+	$years = 10;
+	$year_counter = 0;
+
+	function moneyGained(){
+		global $years;
+		global $year_counter;
+		global $interest;
+		global $money;
+
+		if ( $year_counter < $years) {
+			++$year_counter;
+			$money *= $interest;
+			$money = round($money);
+			echo 'Na ' . $year_counter . ' heeft hij ' . $money . "<br>";
+			moneyGained();
+	
+		}
+
+		return $money;
+	}
+
+moneyGained();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Recursive</title>
+
+
+	<link rel='stylesheet' href="http://web-backend.local/css/global.css">
+	<link rel='stylesheet' href="http://web-backend.local/css/facade.css">
+	<link rel='stylesheet' href="http://web-backend.local/css/directory.css">
+</head>
+<body>
+	
+
+</body>
+</html>
