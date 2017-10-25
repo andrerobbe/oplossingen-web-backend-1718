37f3326dcbc438e0cb261c4e792d3687d5780596
andrerobbe <mauritsrobbe@hotmail.com>
2017-10-25 18:23:11 +0200

    added date

--- date/date.php
+++ date/date.php
@@ -1,5 +1,7 @@
 <?php
-
+	setlocale( 'LC_ALL', 'nld_nld' );
+    $time = mktime("22","35","25","01","21","1904");
+    $time_fix = strftime('%d %B %Y, %H:%M:%S %p', $time);
 
 
 ?>
@@ -8,14 +10,14 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Template</title>
+	<title>Date</title>
 
 	<link rel='stylesheet' href="http://web-backend.local/css/global.css">
 	<link rel='stylesheet' href="http://web-backend.local/css/facade.css">
 	<link rel='stylesheet' href="http://web-backend.local/css/directory.css">
 </head>
 <body>
-	
+	<?php echo $time_fix ?>
 
 </body>
 </html>
