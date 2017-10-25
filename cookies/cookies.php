60b72097edd9e6e02fdff4945b866e9d2f501df2
andrerobbe <mauritsrobbe@hotmail.com>
2017-10-25 19:32:55 +0200

    modified cookies

--- cookies/cookies.php
+++ cookies/cookies.php
@@ -4,30 +4,26 @@
 	if( isset($_GET['logout']) ){
 
 		if($_GET['logout'] == '1'){
+			#delete cookie
 			setcookie('username', "", time() - 1000 ); 
 			setcookie('password', "", time() - 1000 );
 			header('location: cookies.php');
 		}
 	}
 
-	$cookieDuration = 360;
+	$cookieDuration = 300;
 	$myfile = file_get_contents("bestand.txt");
 	$fileArray = explode(",",$myfile);
 	$valid = true;
 	$fout = false;
 
-	if( isset($_POST['onthouden']) == 'onthouden' ){
-		$cookieDuration = 2592000; #30dagen
-	}
-
 	if (isset($_POST["user"]) ) {
 		if($_POST["user"] == $fileArray[0] && $_POST["pw"] == $fileArray[1]) {
+			$valid = false;
+			setcookie('username', $fileArray[0], time() + $cookieDuration );
+			setcookie('password', $fileArray[1], time() + $cookieDuration );
 
-		$valid = false;
-		setcookie('username', $fileArray[0], time() + $cookieDuration );
-		setcookie('password', $fileArray[1], time() + $cookieDuration );
-
-		$loggedIn = ( isset($_COOKIE['username']) ) ? 'Hallo ' . $_POST['user'] . ', fijn dat je er weer bent' : '';
+			$loggedIn = ( isset($_COOKIE['username']) ) ? $_POST['user'] : '';
 		}
 		else {
 			$fout = true;
@@ -48,18 +44,19 @@
 	<link rel='stylesheet' href="http://web-backend.local/css/directory.css">
 </head>
 <body>
-	<?php if(!$valid): ?>
-		<?php if( isset($_COOKIE['username']) ): ?>
-			<h1><?php echo $loggedIn ?></h1>
-		<?php else: ?>
-			<p>U bent ingelogd.</p>
-		<?php endif ?>
-		<a href="cookies.php?logout=1">Uitloggen</a>
-	<?php endif; ?>
+	<?php if(!$valid) { ?>
+		<?php if( isset($_COOKIE['username']) ) {
+			echo '<h1>Hallo ' . $loggedIn . '!</h1>';
+		}
+		else {
+			echo '<p>U bent ingelogd.</p>';
+		}
+		echo '<a href="cookies.php?logout=1">Uitloggen</a>';
+	} ?>
 
-	<?php if($fout): ?>
-		<p>De gebruikersnaam en/of wachtwoord is fout.</p>
-	<?php endif; ?>
+	<?php if($fout) {
+		echo '<p>De gebruikersnaam en/of wachtwoord is fout.</p>';		
+	} ?>
 
 	<?php if($valid): ?>
 		<form action="cookies.php" method="post">
