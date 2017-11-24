1f870bed80cb8804e66d00c10a7c6086e6504b51
andrerobbe <mauritsrobbe@hotmail.com>
2017-11-24 12:16:45 +0100

    add crud-query-1

---
+++ crud-query/query-1.php
@@ -0,0 +1,79 @@
+<?php
+	$error_msg = '';
+
+	try {
+		$db = new PDO('mysql:host=localhost; dbname=bieren', 'root', '');
+		$error_msg = 'Connection to database succesful';
+
+
+		$queryString = 'SELECT *
+								FROM bieren 
+								INNER JOIN brouwers
+								ON bieren.brouwernr = brouwers.brouwernr
+								WHERE bieren.naam LIKE "Du%"
+								AND brouwers.brnaam LIKE "%a%"';
+
+		$statement = $db->prepare($queryString);
+		$statement->execute();
+		$fetchAssoc = array();
+
+		$row = $statement->fetchAll(PDO::FETCH_ASSOC);
+	
+	}
+	catch (PDOException $e){
+		$error_msg = 'Connection to database failed: ' . $e->getMessage();
+
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>query-1</title>
+
+	<link rel='stylesheet' href="http://web-backend.local/css/global.css">
+	<link rel='stylesheet' href="http://web-backend.local/css/facade.css">
+	<link rel='stylesheet' href="http://web-backend.local/css/directory.css">
+	<style>
+		tbody tr:nth-child(2n + 1), thead{
+			background-color: #F0F0F0;
+		}
+
+	</style>
+
+</head>
+<body>
+	<a href="../">home</a>
+	<p><?= $error_msg ?></p>
+
+	<table>
+		<thead>
+			<tr>
+				<?php 
+					echo "<tr>";
+					foreach($row[0] as $key3 => $bier) {
+						echo "<th>". $key3  . "</th>";
+					}
+					echo "</tr>";
+				?>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			foreach($row as $key => $val) {	
+				echo "<tr>";
+				foreach($val as $key2) {
+					echo "<td>". $key2 ."</td>";
+				}
+				echo "</tr>";
+			}
+			?>
+		</tbody>
+		<tfoot>
+			
+		</tfoot>
+	</table>
+</body>
+</html>
---
+++ crud-query/query-2.php
@@ -0,0 +1,44 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>query-1</title>
+
+	<link rel='stylesheet' href="http://web-backend.local/css/global.css">
+	<link rel='stylesheet' href="http://web-backend.local/css/facade.css">
+	<link rel='stylesheet' href="http://web-backend.local/css/directory.css">
+	<style>
+		tbody tr:nth-child(2n + 1), thead{
+			background-color: #F0F0F0;
+		}
+
+	</style>
+
+</head>
+<body>
+	<a href="../">home</a>
+	<p><?= $error_msg ?></p>
+
+	<table>
+		<thead>
+			<tr>
+
+			</tr>
+		</thead>
+		<tbody>
+
+
+		</tbody>
+		<tfoot>
+			
+		</tfoot>
+	</table>
+	
+
+</body>
+</html>
