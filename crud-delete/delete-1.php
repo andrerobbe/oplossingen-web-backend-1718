0409e919af0bb28988e285416ad1595a65a38339
andrerobbe <mauritsrobbe@hotmail.com>
2017-12-01 10:24:35 +0100

    added crud-delete-1

--- crud-delete/delete-1.php
+++ crud-delete/delete-1.php
@@ -1,18 +1,35 @@
 <?php
 	$msg = '';
 
-	if ( isset($_POST[ 'submit' ] ) ) {
-		try {
-			$db = new PDO('mysql:host=localhost; dbname=bieren', 'root', '');
+	try {
+		$db = new PDO('mysql:host=localhost; dbname=bieren', 'root', '');
 
-			$brouwerSelect	=	'SELECT * FROM brouwers';
+		if ( isset($_POST['delete']) ){
+			$brouwerDelete	=	'DELETE FROM brouwers WHERE brouwernr = :brouwernr';
 
-			$statement = $db->prepare( $brouwerDelete );
-		
+			$deleteStatement = $db->prepare( $brouwerDelete );
+			$deleteStatement->bindValue( ':brouwernr', $_POST['delete'] );
+			
+			$isDeleted = $deleteStatement->execute();
+
+			if ( $isDeleted ){
+				$msg = 'Brouwerij succesvol verwijderd';
+			}
+			else{
+				$msg = 'Brouwerij is NIET verwijderd! ' . $deleteStatement->errorInfo()[2];
+			}
 		}
-		catch (PDOException $e){
-			$msg = 'Connection to database failed: ' . $e->getMessage();
-		}
+
+		$brouwerSelect	=	'SELECT * FROM brouwers';
+
+		$statement = $db->prepare( $brouwerSelect );
+		$statement->execute();
+
+		$brouwers = $statement->fetchAll(PDO::FETCH_ASSOC);
+	
+	}
+	catch (PDOException $e){
+		$msg = 'Connection to database failed: ' . $e->getMessage();
 	}
 ?>
 
@@ -26,14 +43,46 @@
 	<link rel='stylesheet' href="http://web-backend.local/css/facade.css">
 	<link rel='stylesheet' href="http://web-backend.local/css/directory.css">
 	<style>
+		tbody tr:nth-child(2n + 1), thead{
+			background-color: #CACACA;
+		}
 	</style>
 </head>
 <body>
 	<a href="../">home</a>
+	<a href="<?= $_SERVER['PHP_SELF'] ?>">reset</a>
 	<p><?php echo $msg ?></p>
-
+	<form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
 	<table>
-		
+		<thead>
+			<tr>
+				<th>#</th>
+				<?php 
+				foreach ($brouwers[0] as $brouwerKey => $brouwerValues) {
+					echo "<th>" . $brouwerKey . "</th>";
+				} ?>
+				<th> </th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php 
+			foreach ($brouwers as $brouwerKey => $brouwerValues) {
+				echo "<tr>";
+					echo "<td>" . $brouwerKey . "</td>";
+					foreach ($brouwerValues as $keyBrouwerVal) {
+						echo "<td>" . $keyBrouwerVal . "</td>";
+					} ?>					
+					<td>
+						<button type="submit" name="delete" value="<?= $brouwerValues['brouwernr'] ?>">
+							<img src="icon-delete.png" alt="Delete button">
+						</button>
+					</td>
+				</tr>
+			<?php } ?>
+		</tbody>
+		<tfoot>
+
+		</tfoot>		
 	</table>
 
 </body>
