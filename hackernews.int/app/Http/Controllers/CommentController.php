54cfe88fb593c2d1258f0cb3fbec341a0a12d84e
andrerobbe <mauritsrobbe@hotmail.com>
2018-01-24 18:15:32 +0100

    added comment page

--- hackernews.int/app/Http/Controllers/CommentController.php
+++ hackernews.int/app/Http/Controllers/CommentController.php
@@ -1,16 +1,21 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use \App\Article;
+use \App\User;
+use App\Rules\ValidateUrl;
 
 class CommentController extends Controller
 {
-	public function show()
+	public function show($id)
 	{
-		return view('comments');
-	}
+		$article = Article::find($id);
+		$user = User::find($id);
 
+		return view('comments')->with('article', $article);
+	}
 
 	
 	public function post()
--- hackernews.int/resources/views/comments.blade.php
+++ hackernews.int/resources/views/comments.blade.php
@@ -7,74 +7,111 @@
         
         <a href="{{ url('/') }}">← back to overview</a>
     </div>
-    <!-- Display Validation Errors -->
-    <!-- resources/views/common/errors.blade.php -->
     
     <div class="panel panel-default">
         <div class="panel-heading clearfix">
-            Article: Scythe, the most-hyped board game of 2016, delivers
+            {{ $article->title }}
         </div>
         <div class="panel-content">
-            <div class="vote">                        
-                <form action="{{ url('/vote/up') }}" method="POST" class="form-inline upvote">
-                    <input type="hidden" name="_token" value="96ZPqN7yznrz9ZUc6fHgUdzfoy0anmbVOac0JMBD">
-                    <button name="article_id"value="1">
-                    <i class="fa fa-btn fa-caret-up" title="upvote"></i>
-                    </button>
-                </form>
-                <form action="{{ url('/vote/down') }}" method="POST" class="form-inline downvote">
-                    <input type="hidden" name="_token" value="96ZPqN7yznrz9ZUc6fHgUdzfoy0anmbVOac0JMBD">
-                    <button name="article_id"value="1">
-                    <i class="fa fa-btn fa-caret-down" title="downvote"></i>
-                    </button>
-                </form>
-            </div>
-            <div class="url">
-                <a href="http://arstechnica.com/gaming/2016/07/scythe-the-most-hyped-board-game-of-2016-delivers/" class="urlTitle">Scythe, the most-hyped board game of 2016, delivers</a>
-            </div>
-            <div class="info">
-                6 points  | posted by Tomte | 2 comments
-            </div>
-            <div class="comments">
-                <ul>
-                    <li>
-                        
-                        <div class="comment-body">Can someone explain to me why it needed to raise $1.8 Million dollars via Kickstarter? Does that cover production costs as well as development time?
-                        Wouldn&#039;t it be more fair to consumers if only development cost was covered by the kickstarter and then production costs were covered by actual pre-orders?</div>
-                        <div class="comment-info">
-                            Posted by eto3 on 2016-08-01 18:46:23
-                        </div>
-                    </li>
-                    <li>
-                        <div class="comment-body">This is one of those games that looks complicated until you start playing. I was one of the Kickstarter backers, and was a bit unsure of the suitability of the game for casual play.
-                        After 22 plays and counting with many different people, I can safely say</div>
-                        <div class="comment-info">
-                            Posted by gasul on 2016-08-01 18:30:35
-                        </div>
-                    </li>
-                </ul>
-            </div>
-            
-            <!-- New Task Form -->
-            <form action="{{ url('/comments/add') }}" method="POST" class="form-horizontal">
-                <input type="hidden" name="_token" value="96ZPqN7yznrz9ZUc6fHgUdzfoy0anmbVOac0JMBD">
-                <!-- Comment data -->
-                <div class="form-group">
-                    <label for="body" class="col-sm-3 control-label">Comment</label>
-                    <div class="col-sm-6">
-                        <textarea type="text" name="body" id="body" class="form-control"></textarea>
-                    </div>
+
+            <!-- votes -->
+             @if (Auth::check())
+                <div class="vote">                        
+                    <form action="{{ url('/vote/up') }}" method="POST" class="form-inline upvote">
+                        {{ csrf_field() }}
+                        <button name="article_id"value="1">
+                        <i class="fa fa-btn fa-caret-up" title="upvote"></i>
+                        </button>
+                    </form>
+
+                    <form action="{{ url('/vote/down') }}" method="POST" class="form-inline downvote">
+                        {{ csrf_field() }}
+                        <button name="article_id"value="1">
+                        <i class="fa fa-btn fa-caret-down" title="downvote"></i>
+                        </button>
+                    </form>
                 </div>
-                <input type="hidden" name="article_id" value="1">
-                <!-- Add comment -->
-                <div class="form-group">
-                    <div class="col-sm-offset-3 col-sm-6">
-                        <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Add comment
+             @else
+                 <div class="vote">
+                    <div class="form-inline upvote">
+                        <button>
+                            <i class="fa fa-btn disabled fa-caret-up" title="Login to vote!"></i>
+                        </button>
+                    </div>
+                    
+                    <div class="form-inline downvote">
+                        <button>
+                            <i class="fa fa-btn disabled fa-caret-down" title="Login to vote!"></i>
                         </button>
                     </div>
                 </div>
-            </form>
+            @endif
+
+            <!-- url -->
+            <div class="url">
+                <a href="{{ $article->url }}" class="urlTitle">{{ $article->title }}</a>
+            </div>
+            <div class="info">
+                {{ $article->votes }} points | posted by {{ $article->posted_by }} | {{ $article->comment->count() }} comments
+            </div>
+
+            <!-- Comment list -->
+            <div class="comments">
+                <ul>
+                    @foreach ($article->comment as $comment)
+                    <li style="list-style-type: none">
+                        <div class="comment-body">{{ $comment->body }}</div>
+                        <div class="comment-info">Posted by {{ $comment->user->name }} on {{ $article->created_at }}</div>
+
+                        <!-- edit/delete button -->
+                        @if (Auth::check())
+                            @if (auth()->user()->id == $comment->user->id)
+                            <div style="display:flex;">
+                                <form action="{{ $comment->id }}/edit" method="GET" style="margin:0 2px 0 0;">
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="btn btn-info">
+                                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                    </button>
+                                </form>
+                                <form action="{{ $comment->id }}/delete" method="GET">
+                                    {{ csrf_field() }}
+                                    <button type="submit" class="btn btn-danger">
+                                        <i class="fa fa-trash" aria-hidden="true"></i>
+                                    </button>
+                                </form>
+                            </div>
+                            @endif
+                        @endif
+                    </li>
+
+                    @endforeach
+                </ul>
+            </div>
+            
+            @if (Auth::check())
+                <!-- New Comment Form -->
+                <form action="{{ url('/comments/add') }}" method="POST" class="form-horizontal">
+                    {{ csrf_field() }}
+                     <!-- Comment data -->
+                    <div class="form-group">
+                        <label for="body" class="col-sm-3 control-label">Comment</label>
+                        <div class="col-sm-6">
+                            <textarea type="text" name="body" id="body" class="form-control"></textarea>
+                        </div>
+                    </div>
+                    <input type="hidden" name="article_id" value="{{ $article->id }}">
+                    <!-- Add comment btn -->
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-6">
+                            <button type="submit" class="btn btn-default">
+                                <i class="fa fa-plus">Add comment</i>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            @else 
+                <p>You need to be <a href="{{ route('login') }}">logged in</a> to comment.</p>
+            @endif
         </div>                               
     </div>
 </div>
